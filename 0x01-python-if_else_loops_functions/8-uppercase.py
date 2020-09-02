f438a635b69b863798b28bada24e9142a18fd231
Justin4587 <Thurman@atlasok.com>
2020-09-01 21:21:54 -0500

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,8 +5,7 @@ def uppercase(str):
         num = ord(element)
         if num > 96 and num < 123:
             newstring += chr(num - 32)
-        elif num > 65 and num < 90:
-                newstring += chr(num - 0)
         else:
                 newstring += chr(num - 0)
     print("{}".format(newstring))
+    print("")
