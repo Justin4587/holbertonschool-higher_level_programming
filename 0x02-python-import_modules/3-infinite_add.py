5d5161f95a8b26c8b5b936d6a556e7c614507dd0
Justin4587 <Thurman@atlasok.com>
2020-09-03 19:45:07 -0500

    add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,11 @@ if __name__ == "__main__":
     import sys
     num = len(sys.argv)
     nums = sys.argv
-    base = sys.argv[1]
     sum = 0
     counter = 1
+    if num == 1:
+        print("{}".format(sum))
+        exit()
     while num > 1:
         sum += int(nums[counter])
         counter = counter + 1
