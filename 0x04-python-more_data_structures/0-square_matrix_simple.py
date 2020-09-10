94bd6321a47e0805d3a8894a7f3d19fef8bf1913
Justin4587 <Thurman@atlasok.com>
2020-09-10 16:35:10 -0500

    maths

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    sq_matrix = []
+    sq = 0
+    for row in matrix:
+        sq_matrix.append(list(map(lambda x: x**2, row)))
+    return (sq_matrix)
---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+
+def search_replace
