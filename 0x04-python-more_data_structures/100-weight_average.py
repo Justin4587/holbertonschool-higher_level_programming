b241d880d6c88b7f073a5aa32a468e4e98022e27
Justin4587 <Thurman@atlasok.com>
2020-09-10 21:40:13 -0500

    avg

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,8 +2,11 @@
 def weight_average(my_list=[]):
     scores = 0
     weights = 0
-    for sw in my_list:
-        scores = scores + sw[0] * sw[1]
-        weights = weights + sw[1]
-    avg = scores / weights
-    return (avg)
+    if my_list != []:
+        for sw in my_list:
+            scores = scores + sw[0] * sw[1]
+            weights = weights + sw[1]
+        avg = scores / weights
+        return (avg)
+    else:
+        return (0)
