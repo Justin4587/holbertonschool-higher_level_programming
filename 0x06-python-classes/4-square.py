f9b2d0a646c6ef9e0493851f72d68dd20ba214c9
Justin4587 <Thurman@atlasok.com>
2020-10-27 20:29:59 -0500

    wrong

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" letter soup """
+
+
+class Square:
+    """ dsdfvbuiarv """
+
+    def __init__(self, size=0):
+        """ base """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """ letter soup """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ definition """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ return area """
+        return self.__size * self.__size
