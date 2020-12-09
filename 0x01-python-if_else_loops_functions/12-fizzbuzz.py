98060bd2e7fa51af737c98125c358e4d6cb4fd8f
Justin4587 <Thurman@atlasok.com>
2020-12-08 23:52:36 -0600

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    a = "Fizz"
+    b = "Buzz"
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print(a + b, end=' ')
+        elif i % 3 == 0:
+            print(a, end=' ')
+        elif i % 5 == 0:
+            print(b, end=' ')
+        else:
+            print(i, end=' ')
