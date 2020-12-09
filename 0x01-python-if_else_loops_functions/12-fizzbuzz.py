42efacf0eeed1b32fd31bf08401bd6cfccffce43
Justin4587 <Thurman@atlasok.com>
2020-12-09 11:29:48 -0600

    pep8 fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     a = "Fizz"
     b = "Buzz"
-    for i in range(1,101):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print(a + b, end=' ')
         elif i % 3 == 0:
