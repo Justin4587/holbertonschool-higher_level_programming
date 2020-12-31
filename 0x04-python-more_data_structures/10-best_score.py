80817fc1b414670c9e926643f132adfb311d9b19
Justin4587 <Thurman@atlasok.com>
2020-12-31 17:43:58 -0600

    name

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     sum = 0
+    name = None
     if a_dictionary:
         key = sorted(a_dictionary.keys())
         for i in key:
             if a_dictionary[i] > sum:
                 sum = a_dictionary[i] + 0
-    return (sum)
+                name = i
+    return (i)
