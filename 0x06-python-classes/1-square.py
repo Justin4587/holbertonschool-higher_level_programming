e31e539645f4ab8fc4881416f0dcf74c10d40dea
Justin4587 <Thurman@atlasok.com>
2021-01-06 10:18:59 -0600

    no square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" I'm not sure how to make a docstring """
+""" I'm not sure how to docstring so lettersoup kjhfgkljahlfkgjha"""
 
 
 class Square:
     """ another one nother one """
 
     def __init__(self, size):
-        self._square__size = size
+        self.__size = size
