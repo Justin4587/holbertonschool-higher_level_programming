c606ff85a1687b809062ce238fa87e9bbcf24d75
Justin4587 <Thurman@atlasok.com>
2021-01-06 13:44:30 -0600

    area

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" I'm not sure how to make a docstring """
+
+
+class Square:
+    """ another one nother one """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ area """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = value
+    
