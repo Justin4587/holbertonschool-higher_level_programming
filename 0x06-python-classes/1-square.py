0f4a6403249d5982fd82c2e6f482b9992a68d49a
Justin4587 <Thurman@atlasok.com>
2021-01-05 23:00:16 -0600

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" I'm not sure how to make a docstring """
+
+
+class Square:
+    """ another one nother one """
+    
+    def __init__(self, size):
+        self._square_size = size
