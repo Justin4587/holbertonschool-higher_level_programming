fe36fd11b954e4264c0766a0d554fe98543f5349
Justin4587 <Thurman@atlasok.com>
2021-01-10 15:18:02 -0600

    tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,10 @@
 def add_integer(a, b=98):
     """ still adding integers """
 
+    if type(a) == float("inf") or type(b) == float("inf"):
+        raise OverflowError("cannot convert float infinity to integer")
+    if type(a) == float("Nan") or type(b) == float("NaN"):
+        raise ValueError("cannot convert float NaN to integer")
     if type(a) == float or type(b) == float:
         a = int(a)
         b = int(b)
@@ -12,6 +16,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) != int:
         raise TypeError("b must be an integer")
-    
 
     return a + b
