250c0c5945afcebc98e9b24299e06df88da9b397
Justin4587 <Thurman@atlasok.com>
2021-01-10 13:10:44 -0600

    tests

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" how many times can this int enter this int """
+
+
+def matrix_divided(matrix, div):
+    """ int penetrators """
+
+    if type(matrix) != list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+
