c6469c5139133405c1ef4243a8149d4ef7bcb1a0
Justin4587 <Thurman@atlasok.com>
2021-01-10 17:26:24 -0600

    matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,8 +4,30 @@
 
 def matrix_divided(matrix, div):
     """ int penetrators """
+    types = ("int", "float")
 
-    if type(matrix) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
-
+    if type(div) == float("inf"):
+        raise OverflowError("cannot convert float infinity to integer")
+    if not isinstance(matrix, list):
+        raise TypeError
+    ("matrix must be a matrix (row of rows) of integers/floats")
+    if type(div) == float("inf"):
+        raise OverflowError("cannot convert float infinity to integer")
+    if type(div) == float("Nan"):
+        raise ValueError("cannot convert float NaN to integer")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for row in matrix:
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError
+            ("matrix must be a matrix (row of rows) of integers/floats")
+    items = len(matrix[0])
+    for row in matrix:
+        if len(row) != items:
+            TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    for row in matrix:
+        for num in row:
+            return ("{:.2f}".format(num/div))
