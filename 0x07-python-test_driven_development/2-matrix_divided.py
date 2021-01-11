4c583a086e52322ea630b18d6100847bf172445e
Justin4587 <Thurman@atlasok.com>
2021-01-11 14:09:49 -0600

    matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,28 +4,27 @@
 
 def matrix_divided(matrix, div):
     """ int penetrators """
-    new_matrix = []
 
-    if type(div) == float("inf"):
-        raise OverflowError("cannot convert float infinity to integer")
-    if not isinstance(matrix, list):
-        raise TypeError
-    ("matrix must be a matrix (row of rows) of integers/floats")
+
     if type(div) == float("inf"):
         raise OverflowError("cannot convert float infinity to integer")
     if type(div) == float("Nan"):
         raise ValueError("cannot convert float NaN to integer")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (row of rows) of integers/floats")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     for row in matrix:
         for num in row:
             if not isinstance(num, (int, float)):
-                raise TypeError
-            ("matrix must be a matrix (row of rows) of integers/floats")
+                raise TypeError("matrix must be a matrix (row of rows) of integers/floats")
     items = len(matrix[0])
-    for row in matrix:
-        if len(row) != items:
+    rows = len(matrix) - 1
+    while rows:
+        if len(matrix[rows]) != items:
+            print("hello")
             TypeError("Each row of the matrix must have the same size")
+            rows = rows - 1
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     
