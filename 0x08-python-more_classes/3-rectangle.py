25347dceaf8e524e911fc4d34cbc6620460e5860
Justin4587 <Thurman@atlasok.com>
2021-01-11 19:29:33 -0600

    NOT SQUARES

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,19 +3,13 @@
 
 
 class Rectangle:
-
     """ This defines the not square """
 
-    number_of_instances = 0
-    print_symbol = "#"
-
     def __init__(self, width=0, height=0):
-
         """really gettin into it now, gonna define the shit out of notsquare"""
 
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -52,27 +46,7 @@ class Rectangle:
     def __str__(self):
         """ mns = my not square """
         mns = ""
-        self.print_symbol = str(self.print_symbol)
-        if self.perimeter == 0:
-            return mns
         for i in range(self.__height - 1):
-            mns += (self.print_symbol * self.__width) + "\n"
-        mns += (self.print_symbol * self.__width)
+            mns += ("#" * self.__width) + "\n"
+        mns += ("#" * self.__width)
         return mns
-
-    def __repr__(self):
-        return ("Rectangle({},{})".format(self.__width, self.__height))
-
-    def __del__(self):
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
-
-    def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
-            return rect_2
