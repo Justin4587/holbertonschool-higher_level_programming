67117d411cdcabbbd012d0f93018e6641d733bd3
Justin4587 <Thurman@atlasok.com>
2021-01-19 23:37:30 -0600

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""base"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+class Square(Rectangle):
+    """4 fucking sides"""
+
+
+    def __init__(self, size):
+        """init"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
