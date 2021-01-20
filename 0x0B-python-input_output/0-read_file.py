adb04b9f4c315d0ddcc6fc404d7b5b9392b067f7
Justin4587 <Thurman@atlasok.com>
2021-01-20 17:56:19 -0600

    sad

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """open and read"""
 
-    with open(filename) as f:
+    with open(filename, encoding="UTF8") as f:
         working_file = f.read()
         print(working_file)
