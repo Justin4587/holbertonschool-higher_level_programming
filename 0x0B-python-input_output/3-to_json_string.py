48652561640ecb62ea568a24f58493859617d7ba
Justin4587 <Thurman@atlasok.com>
2021-01-20 18:45:41 -0600

    sad

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import json
 """json import and rep letterasdkjlghjfb iuahsfdgu"""
-
+import json
 
 def to_json_string(my_obj):
     """json return repasdfg a asfgf  asfdgsdasfg  """
