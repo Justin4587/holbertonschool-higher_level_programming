04e92641bbb7468f67663770f2cc980bcb247adc
Justin4587 <Thurman@atlasok.com>
2021-03-02 21:40:11 -0600

    sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Stuff blah
 -- an extra line for stuff
-SHOW GRANTS FOR user_0d_1 @localhost
-SHOW GRANTS FOR user_0d_2 @localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
