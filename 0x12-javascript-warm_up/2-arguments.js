cc52731409f2da4491729b20ad312008d1256742
Justin4587 <Thurman@atlasok.com>
2021-03-22 22:58:34 -0500

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,14 +2,14 @@
 
 const myVar = ['No argument', 'Argument found', 'Arguments found'];
 
-let arg_list = process.argv;
+const arglist = process.argv;
 
-let i = arg_list.length;
+const i = arglist.length;
 
 if (i <= 2) {
-    console.log(myVar[0]);
+  console.log(myVar[0]);
 } else if (i === 3) {
-    console.log(myVar[1]);
-} else { 
-    console.log(myVar[2])
+  console.log(myVar[1]);
+} else {
+  console.log(myVar[2]);
 }
