be75d5cb38c1cce11c2423c658328b7e4ca8c1bc
Justin4587 <Thurman@atlasok.com>
2021-03-27 01:22:15 -0500

    all

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" I'm going to be mad """
 
 if __name__ == "__main__":
     from sys import argv
@@ -7,7 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect('localhost', argv[1],
                          argv[2], argv[3], port=3306)
     curse = db.cursor()
-    curse.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    curse.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+                   ORDER BY states.id ASC")
     rows = curse.fetchall()
     for row in rows:
         if row[1][0] == 'N':
