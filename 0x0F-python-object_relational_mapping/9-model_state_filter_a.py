2198db216f19c67c1be26f8af93e8e6d6dd8bff6
Justin4587 <Thurman@atlasok.com>
2021-03-27 16:41:49 -0500

    asdfgs

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" I'm going to be mad """
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for states in session.query(State).order_by(
+                State.id).filter(State.name.ilike('%a%')):
+        print("{}: {}".format(states.id, states.name))
+
+    session.close()
