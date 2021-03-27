e2f4033a9faa5b281342fcd900a7aabc75b66372
Justin4587 <Thurman@atlasok.com>
2021-03-27 01:14:35 -0500

    all

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" I'm going to be mad """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect('localhost', argv[1],
+                         argv[2], argv[3], port=3306)
+    curse = db.cursor()
+    curse.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    rows = curse.fetchall()
+    for row in rows:
+        print(row)
+
+    curse.close()
+    db.close()
