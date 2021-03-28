90532669a692d6d7f3711c1309b293d7d5d04a9e
Justin4587 <Thurman@atlasok.com>
2021-03-27 21:59:42 -0500

    update

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" I'm going to be mad """
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(State).filter(
+        State.id == 2).update({State.name: 'New Mexico'})
+    session.commit()
+
+    session.close()
