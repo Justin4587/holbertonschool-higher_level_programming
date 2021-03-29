b4d739c00ec264d180095df59aa03e31f6cbcc40
Justin4587 <Thurman@atlasok.com>
2021-03-28 23:28:10 -0500

    asdgdf

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" I'm going to be mad """
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from sys import argv
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    all = session.query(State).order_by(State.id).all()
+
+    for row in all:
+        print("{}: {} -> {}".format(row.id, row.name, row.state.name))
+
+    session.commit()
+
+    session.close()
