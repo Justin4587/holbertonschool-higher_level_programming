8a90394e707809eb2175f187fd4418f5051e65a7
Justin4587 <Thurman@atlasok.com>
2021-04-10 11:21:44 -0500

    Asdf

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" I'm going to put a comment here"""
+
+if __name__ == "__main__":
+    from urllib import requests
+
+    resp = requests.get("https://intranet.hbtn.io/status")
+    response = resp.text
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
