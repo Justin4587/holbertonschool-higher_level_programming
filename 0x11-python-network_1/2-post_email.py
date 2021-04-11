dab3e506c5b137543f016693a02af10940099fbf
Justin4587 <Thurman@atlasok.com>
2021-04-10 19:52:04 -0500

    Asdf

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,7 +4,7 @@
 if __name__ == "__main__":
     from urllib import request, parse
     from sys import argv
-    bulk = parse.urlencode({"email": argv[2]})
+    bulk = parse.urlencode({"email": argv[2]}).encode("utf-8")
 
     with request.urlopen(argv[1], bulk) as resp:
         print(resp.read().decode("utf-8"))
