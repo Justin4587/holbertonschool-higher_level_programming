b66ded337fdc764ff498b67e47527b76b867b52f
Justin4587 <Thurman@atlasok.com>
2021-04-10 19:37:35 -0500

    Asdf

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,11 +6,13 @@ if __name__ == "__main__":
     from sys import argv
 
     j = 0
-    resp = requests.get("https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1]))
+    resp = requests.get("https://api.github.com/repos/{}/{}/commits".
+                        format(argv[2], argv[1]))
     dict_new = {}
     dict_new = resp.json()
     for i in dict_new:
-        print("{}: {}".format(i.get('sha'),i.get('commit').get('author').get('name')))
+        print("{}: {}".format(i.get('sha'),
+                              i.get('commit').get('author').get('name')))
         j += 1
         if j == 10:
             break
