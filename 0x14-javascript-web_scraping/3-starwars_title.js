53fa2350bd29c1ada3a3197dcc9d7454e2cea42d
Justin4587 <Thurman@atlasok.com>
2021-05-11 20:12:42 -0500

    asdf

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const thewars = 'https://swapi-api.hbtn.io/api/films/' + (process.argv[2]);
+
+request(thewars, (err, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
