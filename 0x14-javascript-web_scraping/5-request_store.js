1db1bbf111a33585d626f5d45107ae96cf31ffa7
Justin4587 <Thurman@atlasok.com>
2021-05-17 14:14:36 -0500

    5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,8 +1,18 @@
 #!/usr/bin/node
 
 const request = require('request');
+const fs = require('fs');
+const theurl = process.argv[2];
+const thepath = process.argv[3];
 
-request(process.argv[2], (err, response) => {
-  if (err) console.log(err);
-  else console.log('code: ' + response.statusCode);
+request(theurl, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(thepath, body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 });
