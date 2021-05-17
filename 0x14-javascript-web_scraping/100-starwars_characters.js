4f244897ba39f4395cdea96e0cb0d474fc7af2c2
Justin4587 <Thurman@atlasok.com>
2021-05-17 14:34:28 -0500

    Asdf

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,8 +1,20 @@
 #!/usr/bin/node
 
 const request = require('request');
+const thewars = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(process.argv[2], (err, response) => {
-  if (err) console.log(err);
-  else console.log('code: ' + response.statusCode);
+request(thewars, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    JSON.parse(body).characters.forEach(character => {
+      request(character, (err, res, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
 });
